d5fad5c7fda31dcb8bf3813392febecb92b01efb
zan-luka <zanluka.nusdorfer@gmail.com>
2020-11-21 16:36:45 +0100

    Moved to Index

--- Gorjanc/wwwroot/js/site.js
+++ Gorjanc/wwwroot/js/site.js
@@ -2,9 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-$( document ).ready(function() {
-    var map = L.map('map').setView([46.1722, 14.9881], 9);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-})
