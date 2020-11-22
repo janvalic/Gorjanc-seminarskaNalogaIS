2a7ae8795846cb2218b67b87679b1121cbd96ca6
janvalic <valicjan@gmail.com>
2020-11-22 14:37:38 +0100

    Edit registration page.

--- Gorjanc/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Gorjanc/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -47,6 +47,16 @@ namespace Gorjanc.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [StringLength(32, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+            [Display(Name = "Name")]
+            public string Name { get; set; }
+
+            [Required]
+            [StringLength(32, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+            [Display(Name = "Surname")]
+            public string Surname { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -75,7 +85,7 @@ namespace Gorjanc.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new Uporabnik { UserName = Input.Email, Email = Input.Email };
+                var user = new Uporabnik { Ime=Input.Name, Priimek=Input.Surname, UserName = Input.Email, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
