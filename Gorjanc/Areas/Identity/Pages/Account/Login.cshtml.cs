7ee40e804068e273a7c3c3dd67a6f8182b18f84a
janvalic <valicjan@gmail.com>
2020-11-24 10:27:37 +0100

    Some translates.

--- Gorjanc/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Gorjanc/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -51,7 +51,7 @@ namespace Gorjanc.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Zapomni me")]
             public bool RememberMe { get; set; }
         }
 
@@ -83,7 +83,7 @@ namespace Gorjanc.Areas.Identity.Pages.Account
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User logged in.");
+                    _logger.LogInformation("Uporabnik prijavljen.");
                     return LocalRedirect(returnUrl);
                 }
                 if (result.RequiresTwoFactor)
@@ -92,12 +92,12 @@ namespace Gorjanc.Areas.Identity.Pages.Account
                 }
                 if (result.IsLockedOut)
                 {
-                    _logger.LogWarning("User account locked out.");
+                    _logger.LogWarning("Uporabniški račun je zaklenjen.");
                     return RedirectToPage("./Lockout");
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    ModelState.AddModelError(string.Empty, "Nepravilen poskus prijave.");
                     return Page();
                 }
             }
--- Gorjanc/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Gorjanc/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -30,7 +30,7 @@ namespace Gorjanc.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out.");
+            _logger.LogInformation("Uporabnik odjavljen.");
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
