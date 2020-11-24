8e6cabe59442eb6d8b8eb5c4d9e73924bf5402b6
zan-luka <zanluka.nusdorfer@gmail.com>
2020-11-24 11:00:37 +0100

    Added Obiskan function

--- Gorjanc/Models/Obisk.cs
+++ Gorjanc/Models/Obisk.cs
@@ -5,7 +5,7 @@ namespace Gorjanc.Models
     public class Obisk
     {
         public int Id { get; set; }
-        public int OsebaId { get; set; }
+        public string OsebaId { get; set; }
         public int VrhId { get; set; }
         public DateTime Datum { get; set; }
 
