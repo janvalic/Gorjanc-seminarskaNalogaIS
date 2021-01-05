81a26fadc78cc092f6f03a7b6d55b3ba71b84837
janvalic <valicjan@gmail.com>
2021-01-05 18:18:51 +0100

    Revert "Fixed API documentation."
    This reverts commit 4fc94ece25fdecc4692def48ea62941bd5bf7a50.

--- Gorjanc/Startup.cs
+++ Gorjanc/Startup.cs
@@ -66,7 +66,7 @@ namespace Gorjanc
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gorjanc API V1");
+                c.SwaggerEndpoint("/api/v1/swagger.json", "Gorjanc API V1");
             });
             app.UseRouting();
 
